5bd65d2b1d5444bc8d48f4e0732a1cf583d3e001
Yemi Adebayo <45774145+yem82@users.noreply.github.com>
2019-04-04 19:34:36 +0100

    Update random-greeting.rb

--- random-greeting.rb
+++ random-greeting.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
-greeting = ["hello", "hi", "what's up", "yo", "ciao", "bonjour", "hola"].sample
-puts greeting
+puts ["hello", "hi", "what's up", "yo", "ciao", "bonjour", "hola"].sample
+
